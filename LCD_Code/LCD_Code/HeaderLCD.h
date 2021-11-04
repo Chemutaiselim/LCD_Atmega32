3976595b98f6e0e7fc7e2390097c574c2888ea77
Selim <selimchemutai19@gmail.com>
2021-11-04 22:33:58 +0300

    Update HeaderLCD.h

--- LCD_Code/LCD_Code/HeaderLCD.h
+++ LCD_Code/LCD_Code/HeaderLCD.h
@@ -9,7 +9,13 @@
 #ifndef HEADERLCD_H_
 #define HEADERLCD_H_
 
-
+/*Function Declarations*/ 
+void LCD_Cmd(unsigned char cmd); 
+void LCD_Char(unsigned char char_data); 
+void LCD_Init(void); 
+void LCD_Clear(void);
+void LCD_String(char *str); 
+void LCD_String_xy(char row, char pos, char *str);
 
 
 
