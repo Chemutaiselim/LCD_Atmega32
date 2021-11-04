1d27ca92b0055162edd2277fbc8c64170e473f26
Selim <selimchemutai19@gmail.com>
2021-11-04 22:01:56 +0300

    Create main.c

---
+++ LCD_Code/LCD_Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD_Code.c
+ *
+ * Created: 04/11/2021 21:58:00
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
