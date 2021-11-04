1035772a5c44fa525e0aba28a87a264a96f73da7
Selim <selimchemutai19@gmail.com>
2021-11-04 22:33:47 +0300

    Update main.c

--- LCD_Code/LCD_Code/main.c
+++ LCD_Code/LCD_Code/main.c
@@ -5,14 +5,17 @@
  * Author : Default
  */ 
 
-#include <avr/io.h>
 
+#define F_CPU 8000000UL 
+#include <avr/io.h>  
+#include <util/delay.h>
+#include "HeaderLCD.h"
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+/*Our main program*/ 
+int main(void) { 
+	LCD_Init(); /* Initialize LCD */ 
+	LCD_String("MSP II:EMT 3202"); /* Write a string on 1st line of LCD*/ 
+	LCD_Cmd(0xC0); /* Go to 2nd line*/ 
+	LCD_String("Hello World"); /* Write string on 2nd line*/
 }
 
